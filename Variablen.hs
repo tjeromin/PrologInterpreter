ff681cbb9adebda5094c24a60dbb6036d0dabf59
DataHamster <robin.mithoff@gmail.com>
2021-03-03 18:44:44 +0100

    variablen.hs unkommentiert

--- Variablen.hs
+++ Variablen.hs
@@ -1,24 +1,22 @@
+import           Data.List
 import           Type
 
 class Vars a where
   allVars :: a -> [VarName]
 
 instance Vars Term where
-  allVars []              = []
-  allVars (Var x)         = [x]
-  allVars (Comb _ y : ys) = nub (allVars y ++ allVars ys)
+  allVars (Var x)           = [x]
+  allVars (Comb _ (y : ys)) = nub (allVars y ++ concatMap allVars ys)
+  allVars (Comb _ [])       = []
 
 instance Vars Rule where
-  allVars []        = []
-  allVars Rule x xs = nub (allVars x ++ allVars xs)
+  allVars (Rule y xs) = nub (allVars y ++ concatMap allVars xs)
 
 instance Vars Prog where
-  allVars []            = []
-  allVars (Prog x : xs) = nub (allVars x ++ allVars xs)
+  allVars (Prog xs) = nub $ concatMap allVars xs
 
 instance Vars Goal where
-  allVars []            = []
-  allVars (Goal x : xs) = nub (allVars x ++ allVars xs)
+  allVars (Goal xs) = nub $ concatMap allVars xs
 
 freshVars :: [VarName]
-freshVars = [VarName (i, j) | j <- [1 .. 100], i <- ['A' .. 'Z']]
+freshVars = [[i] | i <- ['A' .. 'Z']] ++ [i : show j | j <- [0 ..], i <- ['A' .. 'Z']]
