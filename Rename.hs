329787b4b91c0a89831d6dc2d6b28d0b8c888aae
tjeromin <davidjer@hotmail.de>
2021-03-07 22:24:30 +0100

    rename 3

--- Rename.hs
+++ Rename.hs
@@ -7,6 +7,7 @@ module Rename
 import Data.List
 
 import Test.QuickCheck
+import Data.List.Unique
 
 import Type
 import Variables
@@ -15,65 +16,71 @@ import Variables
 rename :: [VarName] -> Rule -> Rule
 rename xs (Rule t ts) 
   = Rule 
-      (head (rList xs (allVars $ Rule t ts) (t:ts))) 
-      (tail (rList xs (allVars $ Rule t ts) (t:ts))) 
+      (head (check_ xs (concatMap allVars $ rList xs (allVars $ Rule t ts) (t:ts)) $ rList xs (allVars $ Rule t ts) (t:ts)))
+      (tail (check_ xs (concatMap allVars $ rList xs (allVars $ Rule t ts) (t:ts)) $ rList xs (allVars $ Rule t ts) (t:ts))) 
  where
   -- Checks if the given variable is not in the given list or rule.
   valid :: [VarName] -> VarName -> Bool
   valid fs v = notElem v fs && notElem v (allVars $ Rule t ts)
 
-  rListAnonym :: [VarName] -> [Term] -> [Term]
-  rListAnonym _  []       = []
-  rListAnonym fs (t1:ts1) = (snd $ rAnonym fs t1) : (rListAnonym ((fst $ rAnonym fs t1) ++ fs) (rListAnonym fs ts1))
-
-  rAnonym :: [VarName] -> Term -> ([VarName], Term)
-  rAnonym fs (Var (VarName v))
-    | head v == '_' = ([nextValid fs (VarName v)], Var $ nextValid fs (VarName v))
-    | otherwise     = ([], Var $ VarName v)
-  rAnonym fs (Comb c terms) = ([], Comb c $ rListAnonym fs terms)
-
-  r :: [VarName] -> [Term] -> [Term]
-  r fs terms 
-    | fst (f fs terms) == VarName "" = terms
-    | otherwise                      = r (fst (f fs terms) : fs) (snd (f fs terms))
-
-  f :: [VarName] -> [Term] -> (VarName, [Term])
-  f _  []                        = (VarName "", [])
-  f fs (y:ys) 
-    | fst (g fs y) == VarName "" = f fs ys 
-    | otherwise                  = (fst (g fs y), (snd (g fs y)) : ys)
-
-  g :: [VarName] -> Term -> (VarName, Term)
-  g fs (Var (VarName "_")) = (nextValid fs (VarName "_"), Var $ nextValid fs (VarName "_"))
-  g fs (Var v)             = (VarName "", Var v)
-  g fs (Comb c terms)      = (fst (f fs terms), Comb c (snd (f fs terms)))
-
+  -- Renames all variables of the second list in all terms of the given third list.
   rList :: [VarName] -> [VarName] -> [Term] -> [Term]
   rList _  []     terms = terms 
-  rList fs (v:vs) terms = rList ((nextValid fs v) : fs) vs (map (rTerm fs v) terms)
+  rList fs (v:vs) terms = rList ((nextValid fs v) : fs) vs (map (rVar fs v) terms)
 
   -- Renames the given variable in a term.
-  rTerm :: [VarName] -> VarName -> Term -> Term
-  rTerm fs v (Var v2)
+  rVar :: [VarName] -> VarName -> Term -> Term
+  rVar fs v (Var v2)
     | v == v2   = Var $ nextValid fs v
     | otherwise = Var v2
-  rTerm fs v (Comb c terms) = Comb c (map (rTerm fs v) terms)
+  rVar fs v (Comb c terms) = Comb c (map (rVar fs v) terms)
 
+  -- Checks if a list of terms contains duplicate variables that begin with '_'.
+  check_ :: [VarName] -> [VarName] -> [Term] -> [Term]
+  check_ fs vs terms 
+    | getDupAnonym vs terms /= [] = check_ fs vs (rList_ fs (head $ getDupAnonym vs terms) terms)
+    | otherwise                   = terms
+
+  -- Renames one occurence of the given variable in the given list.
+  rList_ :: [VarName] -> VarName -> [Term] -> [Term]
+  rList_ _  _ []          = []
+  rList_ fs v (t1:ts1) 
+    | elem v (allVars t1) = (r_ fs v t1 : ts1)
+    | otherwise           = (t1 : rList_ fs v ts1)
+
+  -- Renames one occurence of the given variable in the given term.
+  r_ :: [VarName] -> VarName -> Term -> Term
+  r_ fs _ (Var v2)       = Var $ nextValid fs v2
+  r_ fs v (Comb c terms) = Comb c (rList_ fs v terms)
+
+  -- Renames the given variable such that the new name is valid.
   nextValid :: [VarName] -> VarName -> VarName
   nextValid fs (VarName v) 
-    | length v == 1 = if valid fs (VarName (v ++ "0") )
+    | v == "_"      = VarName "_"
+    | length v == 1 = if valid fs (VarName (v ++ "0"))
                         then VarName (v ++ "0") 
                         else nextValid fs (VarName (v ++ "0"))
     | otherwise     = if valid fs (VarName ((v !! 0) : (countUp v)))
                         then VarName ((v !! 0) : (countUp v))
                         else nextValid fs (VarName ((v !! 0) : (countUp v)))
   
+  -- Increments a variable name by one.
   countUp :: String -> String
   countUp v = show $ (+1) (read [v !! 1] :: Int)
 
--- _ -> _0 -> _1 -> _2 -> ...
+-- Returns a list with all duplicate variables that begin with '_' in all terms 
+-- of the given list.
+getDupAnonym :: [VarName] -> [Term] -> [VarName]
+getDupAnonym fs xs 
+  = filter 
+      (\(VarName x) -> head x == '_' && notElem (VarName x) fs)
+      (repeated $ concatMap allVarsWDup xs)
 
-
+-- Same as allVars but with duplicates.
+allVarsWDup :: Term -> [VarName]
+allVarsWDup (Var x)           = [x]
+allVarsWDup (Comb _ [])       = []
+allVarsWDup (Comb _ (y : ys)) = allVarsWDup y ++ concatMap allVarsWDup ys
 
 
 --------------------------------- QUICKCHECK -----------------------------------
@@ -109,6 +116,20 @@ testAll = $quickCheckAll
 l0 = [VarName "_1", VarName "A"]
 r0 = Rule (Comb "g" [Var (VarName "_"), Var (VarName "_")]) 
           [Var (VarName "_"),Comb "g" [Var (VarName "A"),Var (VarName "B"), Var (VarName "_0")]]
+ts0 = [Comb "g" [Var (VarName "_2"),Var (VarName "_2")], 
+      Var (VarName "_2"),
+      Comb "g" [Var (VarName "A0"),Var (VarName "B0"),Var (VarName "_3")]]
 
 r1 = Rule (Comb "g" [Var (VarName "_"), Comb "g" [Var (VarName "_"), Var (VarName "_")], Var (VarName "_")]) 
           [Var (VarName "_"),Comb "g" [Var (VarName "A"),Var (VarName "B"), Var (VarName "_0")]]
+
+-- (Comb "g" [Var (VarName "_"), Comb "g" [Var (VarName "_"), Var (VarName "_")], Var (VarName "_")])
+
+-- (Comb "g" [Var (VarName "_1"), Comb "g" [Var (VarName "_1"), Var (VarName "_1")], Var (VarName "_1")])
+
+-- check duplicates of _
+-- if list empty return [term]
+-- [VarName "_1"]
+
+-- 
+
