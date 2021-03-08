19d2b67ebb7156975bd364d62ab7dacd79eacde5
datahamster <stu127165@mail.uni-kiel.de>
2021-03-08 13:12:14 +0100

    working Unify.hs without tests.

--- Unification.hs
+++ Unification.hs
@@ -30,13 +30,21 @@ ds (Comb n1 xs) (Comb n2 ys) =
         else ds (head (xs \\ ys)) (head (ys \\ xs))
 
 unify :: Term -> Term -> Maybe Subst
-unify t1 t2 = unifyFull t1 t2 (ds t1 t2) empty
+unify t1 t2 = unifyFull t1 t2 empty
   where
-    unifyFull :: Term -> Term -> Maybe (Term, Term) -> Subst -> Maybe Subst
-    unifyFull t1 t2 (Just (Var v, w)) s = if (elem v w) then Nothing else
-    unifyFull t1 t2  s
+    unifyFull :: Term -> Term -> Subst -> Maybe Subst
+    unifyFull t1 t2 s
       | ds t1 t2 == Nothing = Just s
-      | otherwise = unifyFull (apply (compply s (ds t1 t2)) t1) (apply (compply s (ds t1 t2)) t2) (compply s (ds t1 t2))
+      | otherwise = case ds t1 t2 of
+        (Just (Var v, w)) ->
+          if (elem v (allVars w))
+            then Nothing
+            else unifyFull (apply (compply s (ds t1 t2)) t1) (apply (compply s (ds t1 t2)) t2) (compply s (ds t1 t2))
+        (Just (v, Var w)) ->
+          if (elem w (allVars v))
+            then Nothing
+            else unifyFull (apply (compply s (ds t1 t2)) t1) (apply (compply s (ds t1 t2)) t2) (compply s (ds t1 t2))
+        (Just (Comb n1 xs, Comb n2 ys)) -> Nothing
 
 compply :: Subst -> Maybe (Term, Term) -> Subst
 compply s1 (Just (Var v, w)) = (compose s1 (single v w))
@@ -45,3 +53,11 @@ compply s1 (Just (v, Var w)) = (compose s1 (single w v))
 t1 = Comb "ehemann" [Comb "monika" [], Var (VarName "M")]
 
 t2 = Comb "ehemann" [Var (VarName "F"), Comb "herbert" []]
+
+t3 = Comb "equ" [Comb "f" [Comb "1" []], Comb "g" [Var (VarName "X")]]
+
+t4 = Comb "equ" [Var (VarName "Y"), Var (VarName "Y")]
+
+t5 = Var (VarName "X")
+
+t6 = Comb "f" [Var (VarName "X")]
