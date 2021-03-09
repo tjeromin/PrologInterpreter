eb496ba373d69017d9ed62e1cbcc55ae7ed3fcbf
TinoJer <davidjer@hotmail.de>
2021-03-09 23:44:59 +0100

    SLD

--- SLD.hs
+++ SLD.hs
@@ -1,4 +1,4 @@
-module SLDT
+module SLD
   ( SLDTree
   , Strategy
   , dfs
@@ -18,7 +18,21 @@ data SLDTree = SLDTree Goal [(Subst, SLDTree)]
 -- Represents a search strategy for looking for solutions in a SLD-Tree.
 type Strategy = SLDTree -> [Subst]
 
-sld p g = SLDTree g []
+sld :: Prog -> Goal -> SLDTree
+sld prog (Goal gs) = sld' prog (Goal gs) (allVars prog ++ allVars (Goal gs))
+  where
+    sld' :: Prog -> Goal -> [VarName] -> SLDTree
+    sld' prog (Goal gs) xs = SLDTree (Goal gs) $ map (\(s, g) -> (s, (sld prog g))) (concatMap (goalList prog xs) gs)
+
+unifyCheck :: Rule -> Term -> Maybe (Subst, [Term])
+unifyCheck (Rule r rs) t = case (unify r t) of
+  (Nothing) -> Nothing
+  (Just s)  -> Just (s, (map (\x -> apply s x) rs))
+
+goalList :: Prog -> [VarName] -> Term -> [(Subst, Goal)]
+goalList (Prog (x : xs)) vs t = case (unifyCheck (rename vs x) t) of
+  (Nothing)    -> []
+  Just (s, ys) -> [(s, Goal ys)] ++ goalList (Prog xs) vs t
 
 -- Looks for solutions with depth-first search.
 dfs :: Strategy
